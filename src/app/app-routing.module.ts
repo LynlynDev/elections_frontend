e772c2639db4f37c4b96133751bfb202e6a7eb6a
LynlynDev <reinselynder@gmail.com>
2023-04-26 16:35:57 +0200

    création d'une route vers region_formulaire

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegionComponent } from './components/region/region.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"region_formulaire", component:RegionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
