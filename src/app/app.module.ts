b6097a64e2db20b7094c2b9502a497e940d8ad8b
LynlynDev <reinselynder@gmail.com>
2023-04-26 16:31:04 +0200

    création d'une route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { RegionComponent } from './components/region/region.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    RegionComponent
   ],
   imports: [
     BrowserModule,
