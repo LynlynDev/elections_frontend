ca169721e2418b156428f1387803a5668df31987
LynlynDev <reinselynder@gmail.com>
2023-04-26 14:54:12 +0200

    Mise à jour

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // mettre la vue d'un autre composant
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'elections';
+  title = 'elections_front-end';
+
+  n: number = 15;
+  factoriel(n:number):number{  
+    if (n == 0){
+      return 1;
+    }
+    else {
+      return n * this.factoriel(n-1);
+    }
+    
+  }
 }
