730a287af892351d9cbfb9b1dfa316672a8927f3
LynlynDev <reinselynder@gmail.com>
2023-05-08 19:46:47 +0200

    creation des modals

--- src/app/components/region/region.component.ts
+++ src/app/components/region/region.component.ts
@@ -4,6 +4,7 @@ import { FormBuilder } from '@angular/forms';
 import { region } from 'src/app/model/region.model';
 import { RegionService } from 'src/app/services/region.service';
 
+declare var window:any;
 @Component({
   selector: 'app-region',
   templateUrl: './region.component.html',
@@ -14,29 +15,88 @@ import { RegionService } from 'src/app/services/region.service';
 
 export class RegionComponent implements OnInit{
   public formR!:FormGroup;
+  public formU!:FormGroup;
 
   error?:string
+  region: region = new region();
+  formModal!:any
   success?:string
+  regions?:any //c'est cette variable qui sera interpoler dans la bd pour afficher la liste des régions
   
   constructor(public formBulder: FormBuilder, public service:RegionService){
 
   }
   ngOnInit(): void {
     this.formR = this.formBulder.group({    //formBulder est le nom de la variable et peut -être appéle n'importe comment
-        "label": ["", Validators.required],
+        label: ["", Validators.required],
     });
+    this.formU = this.formBulder.group({    //formBulder est le nom de la variable et peut -être appéle n'importe comment
+      label: ["", Validators.required],
+  });
+    this.formModal=new window.bootstrap.Modal(
+      document.getElementById("lyn")
+    );
   }
 
+  showModal(region:any){
+    this.region= region;
+    this.formModal.show();
+    this.formU.controls['label'].setValue(region.label);
+  }
+
+  updateModal(){
+    
+  }
   onRegisterRegion(){
     const r = new region
     r.label = this.formR.value.label
-    const res = this.service.save(r)
+    const res = this.service.save(r);
+    console.log(res);
 
     if(res!=null){
       this.success = "Enregistrement réussie";
+      this.listeRegion();
     }else{
       this.error ="Echec d'enregistrement";
     }
+  }
+
+
+  listeRegion(){
+    this.service.getlist().subscribe(
+      (data)=>{
+        this.regions=data;
+        console.log(this.regions);
+      },
+      (error)=>{
+        error="impossible d'avoir la liste";
+      })
+  }
+
+  deleteRegion(id:number){
+    this.service.deleteRegion(id).subscribe(
+      (data)=>{
+        this.success="Opération réussie";
+        this.listeRegion();
+      },
+      (error)=>{
+        this.error="Echec de la suppression";
+      }
+    )
+  }
+
+  updateRegion(id:number){
+    this.service.updateRegion(id).subscribe(
+      (data)=>{
+        this.success="Opération réussie";
+        this.listeRegion();
+      },
+      (error)=>{
+        this.error="Echec de la modification";
+      }
+    )
+
+    
   }
 
 
--- src/app/services/region.service.ts
+++ src/app/services/region.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { region } from '../model/region.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,17 +9,30 @@ import { region } from '../model/region.model';
 export class RegionService {
 
 
-  save(region:region):obser{  //region de type region
-    
+  save(region:region):region{  //region de type region
+    //cette fontion permet de créer une région
 
   const req = this.client.post<any>(`${this.CREATE_REGION}`,region); //l'envoie de la requête
    req.subscribe(data =>{ console.log(data); return data },  
             error=>{ console.log(error); return null});
             
-    return null;
+    return region;
              
                   
   }
+
+
+  getlist():Observable<region[]>{  //region de type region      
+      return this.client.get<any>(`${this.CREATE_REGION}`);            
+    }
+
+    deleteRegion(id:number):Observable<region[]>{  //region de type region      
+      return this.client.delete<any>(`${this.CREATE_REGION}/${id}`);            
+    }
+
+    updateRegion(id:number):Observable<region[]>{  //region de type region      
+      return this.client.get<any>(`${this.CREATE_REGION}/${id}`);            
+    }
   
   CREATE_REGION = "http://localhost:8000/api/regionModel";
   constructor(public client:HttpClient) { }
